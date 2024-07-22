384839a696b9553ace32c7ea9b07a87dcf31ca3c
vicky <vigneshkumar1002@gmail.com>
2024-07-23 00:52:41 +0530

    Roboshop redis script

--- MongoDB.sh
+++ MongoDB.sh
@@ -1,5 +1,5 @@
 cp Mongo.repo /etc/yum.repos.d/mongo.repo
 dnf install mongodb-org -y
-##update conf file
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 systemctl enable mongod
 systemctl restart mongod
