c1de7b460b44a5d5ce17a669c3ac0278f05eaa52
vicky <vigneshkumar1002@gmail.com>
2024-07-22 23:01:49 +0530

    Roboshop catalogue script

---
+++ catalogue.sh
@@ -0,0 +1,16 @@
+cp catalogue.service /etc/systemd/system/catalogue.service
+cp Mongo.repo /etc/yum.repos.d/mongo.repo
+dnf module disable nodejs -y
+dnf module enable nodejs:20 -y
+dnf install nodejs -y
+useradd roboshop
+mkdir /app
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip
+cd /app
+unzip /tmp/catalogue.zip
+cd /app
+npm install
+systemctl daemon-reload
+systemctl enable catalogue
+systemctl start catalogue
+dnf install mongodb-mongosh -y
