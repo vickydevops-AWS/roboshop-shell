fa1bd2e25c9dead83cc2801b3e79f9ee163ed7c4
vicky <vigneshkumar1002@gmail.com>
2024-07-23 15:52:19 +0530

    Roboshop cart script

--- catalogue.sh
+++ catalogue.sh
@@ -4,6 +4,7 @@ dnf module disable nodejs -y
 dnf module enable nodejs:20 -y
 dnf install nodejs -y
 useradd roboshop
+rm -rf /app
 mkdir /app
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip
 cd /app
@@ -12,6 +13,6 @@ cd /app
 npm install
 systemctl daemon-reload
 systemctl enable catalogue
-systemctl start catalogue
+systemctl restart catalogue
 dnf install mongodb-mongosh -y
 mongosh --host mongoDB.dev.vickydevops.online </app/db/master-data.js
