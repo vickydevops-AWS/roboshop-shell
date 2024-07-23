6c6668c9fdb5dfc77f4fc572a24f8dd8c218ace5
vicky <vigneshkumar1002@gmail.com>
2024-07-23 16:20:44 +0530

    Roboshop rabbitMQ script

--- rabbitMQ.sh
+++ rabbitMQ.sh
@@ -1,6 +1,6 @@
 cp rabbitMQ.repo /etc/yum.repos.d/rabbitmq.repo
 dnf install rabbitmq-server -y
 systemctl enable rabbitmq-server
-systemctl start rabbitmq-server
+systemctl restart rabbitmq-server
 rabbitmqctl add_user roboshop roboshop123
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
