905920c8890a651d454fca8008a20428fb9cc83e
vicky <vigneshkumar1002@gmail.com>
2024-07-24 23:44:36 +0530

    variables  test

--- catalogue.sh
+++ catalogue.sh
@@ -2,5 +2,8 @@ source common.sh
 component=catalogue
 NODEJS
 
-dnf install mongodb-mongosh -y
-mongosh --host mongoDB.dev.vickydevops.online </app/db/master-data.js
+echo Install MongoDB Client
+dnf install mongodb-mongosh -y >$LOG_FILE
+
+echo Load Master Data
+mongosh --host mongoDB.dev.vickydevops.online </app/db/master-data.js >$LOG_FILE
--- common.sh
+++ common.sh
@@ -1,18 +1,43 @@
+LOG_FILE=/tmp/roboshop.log
+
 NODEJS() {
-  dnf module disable nodejs -y
-  dnf module enable nodejs:20 -y
-  dnf install nodejs -y
-  cp ${component}.service /etc/systemd/system/${component}.service
-  cp Mongo.repo /etc/yum.repos.d/mongo.repo
-  useradd roboshop
-  rm -rf /app
-  mkdir /app
-  curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip
+  echo disable nodeJS Default Version
+  dnf module disable nodejs -y >$LOG_FILE
+
+  echo Enable nodeJS 20 Module
+  dnf module enable nodejs:20 -y >$LOG_FILE
+
+  echo install Nodejs
+  dnf install nodejs -y >$LOG_FILE
+
+  echo copy service file
+  cp ${component}.service /etc/systemd/system/${component}.service >$LOG_FILE
+
+  echo COpy MongoDB repo file
+  cp Mongo.repo /etc/yum.repos.d/mongo.repo >$LOG_FILE
+
+echo Adding Application user
+  useradd roboshop >$LOG_FILE
+
+  echo Cleaning old content
+  rm -rf /app >$LOG_FILE
+
+  echo Create app directory
+  mkdir /app >$LOG_FILE
+
+  echo Download app content
+  curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip >$LOG_FILE
+
   cd /app
-  unzip /tmp/${component}.zip
-  cd /app
-  npm install
-  systemctl daemon-reload
-  systemctl enable ${component}
-  systemctl restart ${component}
+
+echo Extract App content
+  unzip /tmp/${component}.zip >$LOG_FILE
+
+echo download NodeJS Dependencies
+  npm install >$LOG_FILE
+
+echo start start servive
+  systemctl daemon-reload >$LOG_FILE
+  systemctl enable ${component} >$LOG_FILE
+  systemctl restart ${component} >$LOG_FILE
 }
