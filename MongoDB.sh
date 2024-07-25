a50bf4a03e2e6860d1d4dc5cb343c5b2c6231ec1
vicky <vigneshkumar1002@gmail.com>
2024-07-25 21:32:15 +0530

    SHELL SCRIPT

--- MongoDB.sh
+++ MongoDB.sh
@@ -1,5 +1,19 @@
+source common.sh
+component=MongoDB
+
+PRINT Copy MongoDB repo file
 cp Mongo.repo /etc/yum.repos.d/mongo.repo
+STAT $?
+
+PRINT Install MongoDB
 dnf install mongodb-org -y
+STAT $?
+
+PRINT Update MongoDB repo file
 sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+STAT $?
+
+PRINT Start MongoDB service
 systemctl enable mongod
 systemctl restart mongod
+STAT $?
