7ac28e5ebd8cd55a70fd3ff3ad371b1c03815f2d
vicky <vigneshkumar1002@gmail.com>
2024-07-25 22:14:53 +0530

    SHELL SCRIPT

--- shipping.sh
+++ shipping.sh
@@ -9,6 +9,13 @@ unzip /tmp/shipping.zip
 cd /app
 mvn clean package
 mv target/shipping-1.0.jar shipping.jar
+
+dnf install mysql -y
+mysql -h MySQL.dev.vickydevops.online -uroot -pRoboShop@1 < /app/db/schema.sql
+mysql -h MySQLSQL.dev.vickydevops.online -uroot -pRoboShop@1 < /app/db/master-data.sql
+mysql -h MySQL.dev.vickydevops.online -pRoboShop@1 < /app/db/app-user.sql
+
+
 systemctl daemon-reload
 systemctl enable shipping
 systemctl restart shipping
