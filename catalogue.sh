ad5d83dd578d1d8748a752f3714a6da43fca90b5
vicky <vigneshkumar1002@gmail.com>
2024-07-25 19:16:48 +0530

    variables  test

--- catalogue.sh
+++ catalogue.sh
@@ -4,16 +4,16 @@ NODEJS
 
 echo Install MongoDB Client
 dnf install mongodb-mongosh -y &>>$LOG_FILE
-if [$? -eq 0 ]; then
-    echo SUCCESS
-  else
-    echo FAILURE
-  fi
+if [ $? -eq 0 ]; then
+  echo SUCCESS
+else
+  echo FAILURE
+fi
 
 echo Load Master Data
 mongosh --host mongoDB.dev.vickydevops.online </app/db/master-data.js &>>$LOG_FILE
-if [$? -eq 0 ]; then
-    echo SUCCESS
-  else
-    echo FAILURE
-  fi
+if [ $? -eq 0 ]; then
+  echo SUCCESS
+else
+  echo FAILURE
+fi
--- common.sh
+++ common.sh
@@ -11,102 +11,102 @@ PRINT(){
 NODEJS() {
   echo disable nodeJS Default Version
   dnf module disable nodejs -y &>>$LOG_FILE
-  if [$? -eq 0 ]; then
+  if [ $? -eq 0 ]; then
     echo SUCCESS
   else
     echo FAILURE
   fi
 
 
-  echo Enable nodeJS 20 Module
-  dnf module enable nodejs:20 -y &>>$LOG_FILE
- if [$? -eq 0 ]; then
-     echo SUCCESS
-   else
-     echo FAILURE
-   fi
+echo Enable nodeJS 20 Module
+dnf module enable nodejs:20 -y &>>$LOG_FILE
+ if [ $? -eq 0 ]; then
+    echo SUCCESS
+  else
+    echo FAILURE
+  fi
 
   echo install Nodejs
   dnf install nodejs -y &>>$LOG_FILE
-   if [$? -eq 0 ]; then
-     echo SUCCESS
-   else
-     echo FAILURE
-   fi
+  if [ $? -eq 0 ]; then
+    echo SUCCESS
+  else
+    echo FAILURE
+  fi
 
   echo copy service file
   cp ${component}.service /etc/systemd/system/${component}.service &>>$LOG_FILE
-  if [$? -eq 0 ]; then
-      echo SUCCESS
-    else
-      echo FAILURE
-    fi
+  if [ $? -eq 0 ]; then
+    echo SUCCESS
+  else
+    echo FAILURE
+  fi
 
   echo COpy MongoDB repo file
   cp Mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOG_FILE
-  if [$? -eq 0 ]; then
-      echo SUCCESS
-    else
-      echo FAILURE
-    fi
+  if [ $? -eq 0 ]; then
+    echo SUCCESS
+  else
+    echo FAILURE
+  fi
 
   echo Adding Application user
   useradd roboshop &>>$LOG_FILE
-  if [$? -eq 0 ]; then
-      echo SUCCESS
-    else
-      echo FAILURE
-    fi
+  if [ $? -eq 0 ]; then
+    echo SUCCESS
+  else
+    echo FAILURE
+  fi
 
   echo Cleaning old content
   rm -rf /app &>>$LOG_FILE
-  if [$? -eq 0 ]; then
-      echo SUCCESS
-    else
-      echo FAILURE
-    fi
+  if [ $? -eq 0 ]; then
+    echo SUCCESS
+  else
+    echo FAILURE
+  fi
 
   echo Create app directory
   mkdir /app &>>$LOG_FILE
-  if [$? -eq 0 ]; then
-      echo SUCCESS
-    else
-      echo FAILURE
-    fi
+  if [ $? -eq 0 ]; then
+    echo SUCCESS
+  else
+    echo FAILURE
+  fi
 
   echo Download app content
   curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>$LOG_FILE
-  if [$? -eq 0 ]; then
-      echo SUCCESS
-    else
-      echo FAILURE
-    fi
+  if [ $? -eq 0 ]; then
+    echo SUCCESS
+  else
+    echo FAILURE
+  fi
 
   cd /app
 
   echo Extract App content
   unzip /tmp/${component}.zip &>>$LOG_FILE
-  if [$? -eq 0 ]; then
-      echo SUCCESS
-    else
-      echo FAILURE
-    fi
+  if [ $? -eq 0 ]; then
+    echo SUCCESS
+  else
+    echo FAILURE
+  fi
 
   echo download NodeJS Dependencies
   npm install &>>$LOG_FILE
-  if [$? -eq 0 ]; then
-      echo SUCCESS
-    else
-      echo FAILURE
-    fi
+  if [ $? -eq 0 ]; then
+    echo SUCCESS
+  else
+    echo FAILURE
+  fi
 
   echo start servive
   systemctl daemon-reload &>>$LOG_FILE
   systemctl enable ${component} &>>$LOG_FILE
   systemctl restart ${component} &>>$LOG_FILE
-  if [$? -eq 0 ]; then
-      echo SUCCESS
-    else
-      echo FAILURE
-    fi
+  if [ $? -eq 0 ]; then
+    echo SUCCESS
+  else
+    echo FAILURE
+  fi
 }
