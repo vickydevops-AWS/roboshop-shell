72a1fdd8a68044f17afada0acb0f9fa5c05dceaa
vicky <vigneshkumar1002@gmail.com>
2024-07-26 20:31:18 +0530

    SHELL SCRIPT

--- common.sh
+++ common.sh
@@ -125,16 +125,10 @@ SCHEMA_SETUP(){
    dnf install mysql -y &>>$LOG_FILE
    STAT $?
 
-   PRINT Load Schema
-   mysql -h MySQL.dev.vickydevops.online -uroot -pRoboShop@1 < /app/db/schema.sql &>>$LOG_FILE
-   STAT $?
 
-   PRINT Load Master Data
-   mysql -h MySQL.dev.vickydevops.online -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$LOG_FILE
-   STAT $?
-
-   PRINT Create App Users
-   mysql -h MySQL.dev.vickydevops.online -pRoboShop@1 < /app/db/app-user.sql &>>$LOG_FILE
+for file in schema master-data app-user; do
+   PRINT Load file - $file.sql
+   mysql -h MySQL.dev.vickydevops.online -uroot -pRoboShop@1 < /app/db/$file.sql &>>$LOG_FILE
    STAT $?
  fi
 
